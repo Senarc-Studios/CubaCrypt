9296e904616c84b6a58a143fe4b4ba6da8954667
Benitz Original <benitz@numix.xyz>
2021-06-24 20:55:38 +0800

    Revert "Base Starting point - v0.0.1"
    This reverts commit 8fe5879c81fbb476ec12363d01943dc04c6c56ea.

--- src/key_gen.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-def generate(length):
-    # choose from all lowercase letter
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
