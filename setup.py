5f00e46de6582b6dc3e368ab03bf6295a4f65552
Benitz Original <benitz@numix.xyz>
2021-06-25 13:55:30 +0800

    License Update - v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name='cubacrypt',  
-	version='0.0.2',
+	version='0.1.1',
 	repository="https://github.com/BenitzCoding/CubaCrypt",
 	scripts=['cubacrypt'] ,
 	author="Benitz Original",
@@ -14,11 +14,11 @@ setup(
 	description="An custom encryption method made by encrypting with many types Number systems and characters.",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/BenitzCoding/CubaCrypt/releases/tag/v0.0.1",
+	url="https://github.com/BenitzCoding/CubaCrypt/releases/tag/v0.0.2",
 	package_dir={"": "src"},
 	classifiers=[
 		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
 		"Operating System :: OS Independent",
 	],
 	include_package_data=True,
