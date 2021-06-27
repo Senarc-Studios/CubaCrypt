275c17d08c9d724217fc549c0fbcd9c9db9a5d11
Benitz Original <benitz@numix.xyz>
2021-06-27 16:49:45 +0800

    Something...

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from distutils.core import setup
 
-with open("README.md", "r") as fh:
-	long_description = fh.read()
-
-
 setup(
 	name='cubacrypt',  
-	version='0.1.1',
+	version='0.1.2',
 	repository="https://github.com/BenitzCoding/CubaCrypt",
 	scripts=['cubacrypt'] ,
 	author="Benitz Original",
 	author_email="benitz@numix.xyz",
 	description="An custom encryption method made by encrypting with many types Number systems and characters.",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/BenitzCoding/CubaCrypt/releases/tag/v0.0.2",
+	url="https://github.com/BenitzCoding/CubaCrypt/releases/tag/v0.1.2",
 	package_dir={"": "src"},
 	classifiers=[
 		"Programming Language :: Python :: 3",
